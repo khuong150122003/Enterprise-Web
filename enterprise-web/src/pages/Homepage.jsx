53d7bba04103307c0a8b54bc6f8db05c60f44b28
Nhat Khuong <khuongthcstanhung@gmail.com>
2024-11-04 17:36:17 +0700

    fix homepage

--- enterprise-web/src/pages/Homepage.jsx
+++ enterprise-web/src/pages/Homepage.jsx
@@ -1,59 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Classes from "./Classes.jsx";
-import Profile from "./Profile.jsx";
-import Login from "./Login.jsx";
+import "./Homepage.css";
 
-const HomePage = () => {
+function Homepage() {
   return (
-    <div>
-      <div className="logo-container">
-        <img
-          src="/img/logouni.jpg"
-          alt="University Logo"
-          className="logo-image"
-        />
+    <div className="homepage">
+      <img src="/img/homepage.jpg" alt="University" className="hero-image" />
+      <div className="welcome-text">
+        <h1>WELCOME TO</h1>
+        <h2>UNIVERSITY OF GREENWICH</h2>
       </div>
-
-      <header>
-        <Navbar />
-      </header>
-
-      <main>
-        <div className="home-content">
-          <h1>
-            WELCOME TO <br />
-            UNIVERSITY OF GREENWICH
-          </h1>
-          <img
-            src="/img/homepage.jpg"
-            alt="University"
-            className="hero-image"
-          />
-        </div>
-      </main>
     </div>
   );
-};
+}
 
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <ul className="nav-links-left">
-        {/* <li>
-         <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <Classes /> 
-        <Profile />*/}
-      </ul>
-
-      <ul className="nav-links-right">
-        <Login />
-      </ul>
-    </nav>
-  );
-};
-
-export default HomePage;
+export default Homepage;
